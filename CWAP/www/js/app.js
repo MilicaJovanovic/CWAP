fd710de72a6443830a0e4a50a3b8ba50b0813c88
Milica Jovanovic <j.milica95@yahoo.com>
2017-03-21 20:41:20 +0100

    Initial commit

---
+++ CWAP/www/js/app.js
@@ -0,0 +1,22 @@
+var myApp = angular.module('starter', ['ionic', 'ngStorage'])
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginController'
+  });
+
+  $urlRouterProvider.otherwise('/login');
+});
